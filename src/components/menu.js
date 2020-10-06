44ce7708e2974d695c0b14f37cdf0f0a4d034354
skwlx.xx@o2.pl <skwlx.xx@o2.pl>
2020-10-06 11:24:54 +0200

    Finished menu section

--- src/components/menu.js
+++ src/components/menu.js
@@ -12,8 +12,56 @@ const Menu = () => (
                 <img src={tacoJpg} alt="Taco" />
                 <img src={tacosJpg} alt="Taco" />
             </div>
+            <h2>MENU</h2>
             <div className="menuContent">
-                <h2>MENU</h2>
+                <div className="menuFood">
+                    <ul>
+                    <h3>Taco</h3>
+                        <li>Lorem 10 zł</li>
+                        <li>Lorem 10 zł</li>
+                        <li>Lorem 10 zł</li>
+                        <li>Lorem 10 zł</li>
+                        <li>Lorem 10 zł</li>
+                    </ul>
+                </div>
+                <div className="menuFood">
+                <ul>
+                <h3>Burrito</h3>
+                    <li>Lorem 12 zł</li>
+                    <li>Lorem 12 zł</li>
+                    <li>Lorem 12 zł</li>
+                    <li>Lorem 12 zł</li>
+                    <li>Lorem 12 zł</li>
+                </ul>
+                </div>
+                <div className="menuFood">
+                <ul>
+                <h3>Quesadillas</h3>
+                    <li>Lorem 14 zł</li>
+                    <li>Lorem 14 zł</li>
+                    <li>Lorem 14 zł</li>
+                    <li>Lorem 14 zł</li>
+                    <li>Lorem 14 zł</li>
+                </ul>
+                </div>
+                <div className="menuFood">
+                <ul>
+                <h3>Napoje</h3>
+                    <li>Lorem 5 zł</li>
+                    <li>Lorem 5 zł</li>
+                    <li>Lorem 5 zł</li>
+                    <li>Lorem 5 zł</li>
+                    <li>Lorem 5 zł</li>
+                </ul>
+            </div>
+            <div className="menuFood menuFood-a">
+            <ul>
+            <h3>Sosy</h3>
+                <li>Lorem 1 zł</li>
+                <li>Lorem 1 zł</li>
+                <li>Lorem 1 zł</li>
+            </ul>
+        </div>
             </div>
         </div> 
     </div>
